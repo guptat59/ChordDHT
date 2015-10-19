08eb231e664eac0f1334ea08fec92dcdc5aeee28
rakeshdrk <rakeshdrk29@gmail.com>
2015-10-18 20:21:35 -0400

    predec and succ

--- src/main/scala/Project3.scala
+++ src/main/scala/Project3.scala
@@ -3,28 +3,42 @@ import java.security.MessageDigest
 import akka.actor.ActorSystem
 import akka.actor.Props
 import java.lang.Long
+import javafx.scene.paint.Stop
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import akka.actor.ActorRef
+import scala.concurrent.impl.Future
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.event.Logging
+import com.typesafe.config.ConfigFactory
 
-object Project2 {
-
+object Constants {
   val nodePrefix = "Node-"
-  val totalSpace: Int = Integer.MAX_VALUE
+  val m: Int = (math.ceil((math.log10(Integer.MAX_VALUE) / math.log10(2)))).toInt - 1
+  val totalSpace: Int = Math.pow(2, m).toInt
+  val actorSystem = "ChordSys"
+  val namingPrefix = "akka://" + actorSystem + "/user/"
+}
 
-  def getHash(id: String, totalSpace: Long): Int = {
+object Chord {
 
-    // Maximum total hash space can be 2 ^ 60.
+  def getHash(id: String, totalSpace: Int): Int = {
+
+    // Maximum total hash space can be 2 ^ 30.
     if (id != null) {
       var key = MessageDigest.getInstance("SHA-256").digest(id.getBytes("UTF-8")).map("%02X" format _).mkString.trim()
       if (key.length() > 15) {
         key = key.substring(key.length() - 15);
       }
       (Long.parseLong(key, 16) % totalSpace).toInt
-    }
-    0
+    } else
+      0
   }
 
   def main(args: Array[String]): Unit = {
 
-    var numNodes = 5;
+    var numNodes = 4;
     var numRequests = 10;
 
     if (args.length > 0) {
@@ -32,52 +46,87 @@ object Project2 {
       numRequests = args(1).toInt
     }
 
-    val system = ActorSystem("ChordSys")
+    var config = """
+      akka {
+          loglevel = INFO
+      }"""
+    val system = ActorSystem(Constants.actorSystem, ConfigFactory.parseString(config))
 
     // totalSpace = ((math.ceil((math.log10(numNodes) / math.log10(2)))).toInt) * (2 ^ 10);    
     var firstNode: Int = -1;
-
+    var node_1: ActorRef = null;
     for (i <- 1 to numNodes) {
 
       if (firstNode == -1) {
-        firstNode = getHash(nodePrefix + i, totalSpace)
-        var a = system.actorOf(Props(new Peer(firstNode, nodePrefix + i)), firstNode.toString())
-        // Initialize the first node
-        a ! new bootPeer(null)
+        firstNode = getHash(Constants.nodePrefix + i, Constants.totalSpace)
+        node_1 = system.actorOf(Props(new Peer(firstNode, Constants.nodePrefix + i)), firstNode.toString())
+        node_1 ! new join(-1)
+        println("First Node" + firstNode)
       } else {
-        // Use the firstNode as the neighbor to lookup information.
-        var hashName = getHash(nodePrefix + i, totalSpace)
-        var a = system.actorOf(Props(new Peer(hashName, nodePrefix + i)), hashName.toString())
-        a ! new bootPeer(firstNode)
+        var hashName = getHash(Constants.nodePrefix + i, Constants.totalSpace)
+        var node = system.actorOf(Props(new Peer(hashName, Constants.nodePrefix + i)), hashName.toString())
+        node ! new join(firstNode)
+        Thread.sleep(1000)
       }
-    }
 
+      // Initialize the first node
+
+      // Use the firstNode as the neighbor to lookup information.
+    }
+    node_1 ! "print"
   }
 }
 
 sealed trait Seal
-case class bootPeer(myRandNeigh: Long) extends Seal
+case class join(myRandNeigh: Int) extends Seal
+case class initFingerTable() extends Seal
 case class hasFileKey(fileKey: String) extends Seal
+case class getPredec() extends Seal
+case class getSucc(fromNewBie: Boolean) extends Seal
+case class setPredec(name: Int, fromNewbie: Boolean) extends Seal
+case class setSucc(name: Int, fromNewbie: Boolean) extends Seal
+case class findPredecessor(key: Int, origin: Int, isRequest: Boolean) extends Seal
+case class findSuccessor(key: Int, src: Int, isRequest: Boolean) extends Seal
 
 class Peer(val hashName: Int, val abstractName: String) extends Actor {
 
-  var range1: Range = null;
-  var range2: Range = null;
-
+  val log = Logging(context.system, this)
+  // var range1: Range = null;
+  // var range2: Range = null;
+  var fingerTable = new HashMap[Int, Int]
+  var successor: Int = -1;
+  var predecessor: Int = -1;
+  var isJoined: Boolean = false;
   def receive = {
-    case b: bootPeer => {
-      if (b.myRandNeigh == null) {
+    case b: join => {
+
+      if (b.myRandNeigh == -1) {
         // Implies first Node.
-        range1 = 3 to Project2.totalSpace + 1
-        range2 = Project2.totalSpace to hashName
+        //range1 = hashName to Project2.totalSpace - 1
+        //range2 = 0 to hashName-1
+        successor = hashName;
+        predecessor = hashName;
+        fillFingerTable(-1);
+        isJoined = true
       } else {
-        // nth node.
+        var act = context.actorSelection(Constants.namingPrefix + b.myRandNeigh)
+        act ! findPredecessor(hashName, hashName, true)
+
+        // predecessor = findPredecessor(b.myRandNeigh)
+        // successor = findSuccessor(b.myRandNeigh)
+        //  fillFingerTable(predecessor)
+        // updateFingerTable(hashName);
+
+        //actorRef.successor = hashName; setting the successor to new node.
+        // fillFingerTable(hashName)
       }
     }
+    case f: initFingerTable => {
 
+    }
     case h: hasFileKey => {
 
-      var found = false;      
+      /*var found = false;      
       if (range1 != null) {
         found = range1.contains(h.fileKey)
         if (!found) {
@@ -87,7 +136,127 @@ class Peer(val hashName: Int, val abstractName: String) extends Actor {
       if (found) {
         println("Found key " + h.fileKey + " at node " + context.self.path.name)
       }
+    }*/
     }
 
+    case s: getSucc => {
+      log.debug("\tType:getSucc\t\tFrom:" + sender.path + "\tTo:" + hashName + "\t Msg: " + s)
+      if (s.fromNewBie) {
+        sender ! setSucc(successor, false)
+      }
+    }
+
+    case p: getPredec => {
+
+    }
+
+    case s: setSucc => {
+      log.debug("\tType:setSucc\t\tFrom:" + sender.path + "\tTo:" + hashName + "\t Msg: " + s)
+      if (s.fromNewbie && isJoined) {
+        var myOldSuccesor = context.actorSelection(Constants.namingPrefix + successor)
+        successor = s.name
+        myOldSuccesor ! setPredec(s.name, isJoined)
+      } else if (!s.fromNewbie && !isJoined) {
+        successor = s.name
+        //Since I am a new node, Update the successor info of my new predecessor
+        var myNewPredecessor = context.actorSelection(Constants.namingPrefix + predecessor)
+        myNewPredecessor ! setSucc(hashName, !isJoined)
+        var f = Future {
+          Thread.sleep(10)
+        }
+        //f.onComplete { case x => self.tell(initFingerTable(), self) }//println("me" + hashName + " pre: " + predecessor + " Succ: " + successor
+        f.onComplete { case x => self.tell("print", self) }
+
+      }
+    }
+
+    case p: setPredec => {
+      log.debug("\tType:setPredec\t\tFrom:" + sender.path + "\tTo:" + hashName + "\t Msg: " + p)
+      if (isJoined && !p.fromNewbie) {
+        predecessor = p.name;
+      } else if (!isJoined && !p.fromNewbie) {
+        predecessor = p.name
+        var act = context.actorSelection(Constants.namingPrefix + predecessor)
+        act ! getSucc(!isJoined)
+      }
+    }
+
+    case f: findPredecessor => {
+      if (f.isRequest) {
+        log.debug("\tType:findPredecessor\t\tFrom:" + sender.path + "\tTo:" + hashName + "\t Msg: " + f)
+        var start = hashName
+        var end = successor
+        if (start == end) {
+          var act = context.actorSelection(Constants.namingPrefix + f.origin)
+          act ! setPredec(start, false)
+        } else if (f.key >= start && f.key < end) {
+          var act = context.actorSelection(Constants.namingPrefix + f.origin)
+          act ! setPredec(start, false)
+        } else {
+          var closetNeigh = closestPrecedingFinger(f.key);
+          var act = context.actorSelection(Constants.namingPrefix + closetNeigh)
+          act ! findPredecessor(f.key, f.origin, f.isRequest)
+        }
+      }
+    }
+
+    case f: findSuccessor => {
+      var x = self
+    }
+
+    case x: String => {
+      if (x.equals("print")) {
+        println("My name " + hashName + " Pre: " + predecessor + " Succ: " + successor)
+      }
+    }
+
+  }
+
+  //  def fillFingerTable(myRandNeigh: Int, hashName: Int): Unit = {
+  //    if (myRandNeigh == -1) {
+  //      for (i <- 0 until Chord.m ) {
+  //        //range = (hashName + (Math.pow(2, i).toInt % Chord.totalSpace).toInt) to (hashName + (Math.pow(2, i + 1).toInt % Chord.totalSpace).toInt)
+  //        fingerTable(i) = hashName
+  //      }
+  //    } else {
+  //      //update
+  //    }
+  //  }
+
+  def fillFingerTable(myRandNeigh: Int): Unit = {
+    if (myRandNeigh == -1) {
+      for (i <- 0 until Constants.m) {
+        fingerTable(hashName + (Math.pow(2, i).toInt % Constants.totalSpace).toInt) = hashName
+      }
+    } else {
+      //update
+    }
+  }
+
+  def findPredec(key: Int): Int = {
+    1
+  }
+
+  def findSucc(myRandNeigh: Int): Int = {
+    // conext.A
+    1
+  }
+
+  def updateFingerTable(hashName: Int): Int = {
+    1
+  }
+
+  def closestPrecedingFinger(key: Int): Int = {
+    var keyFound = Integer.MIN_VALUE
+    var it = fingerTable.keys.iterator
+    while (it.hasNext) {
+      var current = it.next().toInt
+      if (current < key) {
+        if (keyFound < current) {
+          keyFound = current
+        }
+      }
+    }
+    keyFound
   }
 }
